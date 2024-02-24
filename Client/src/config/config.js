fe65b998887cda31083b566718a3a05f6ea460f3
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2024-02-24 20:08:14 +0100

    Update config.js

--- Client/src/config/config.js
+++ Client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:3000/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://ai-3d-customizer.onrender.com/api/v1/dalle",
   },
 };
 
